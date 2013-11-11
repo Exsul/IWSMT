416c629af6eb38675d3f5f45db014c2de5c22e6d
Kirill Berezin <enelar@develop-project.ru>
2013-11-12 03:28:17 +0400

    Empty activity

--- src/net/exsul/IWSMT/Random.java
+++ src/net/exsul/IWSMT/Random.java
@@ -3,12 +3,12 @@ package net.exsul.IWSMT;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Random extends Activity
-{
-    /** Called when the activity is first created. */
+public class Random extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
